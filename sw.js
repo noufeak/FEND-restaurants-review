a659ed61951ced5a03e9ceda8a8b4a09c38290a0
noufeak <59263578+noufeak@users.noreply.github.com>
2020-02-25 00:18:27 +0300

    fix: Image urls

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
-let cache_name = 'mws-cache-v1';
+const cache_name = 'mws-cache-v1';
 
 //List of URLs to be cached
-let urlsToCache = [
+const urlsToCache = [
   './',
-  './index.html',
-  './restaurant.html',
-  './css/styles.css',
-  './data/restaurants.jason',
-  './img/*',
-  './js/dbhelper.js',
-  './js/main.js',
-  './js/restaurant_info.js',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg',
+  './img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg',
+  '/img/9.jpg', '/img/10.jpg',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
   'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
   'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
   '//normalize-css.googlecode.com/svn/trunk/normalize.css'
@@ -34,7 +36,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map.(cacheName => {
+        cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
@@ -48,7 +50,9 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
     .then(response => {
-      if(response) return response;
+      if(response) {
+        return response;
+      }
       return fetch(event.request);
     })
   );
